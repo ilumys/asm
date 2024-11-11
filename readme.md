e660d5cb750c8b15b0234c0ebf6c0712f5731501
skyaadi <vcs@skyaa.dev>
2024-11-11 19:49:12 +1100

    add hello world; build with clang/lld

--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 # Assembly
 
 For learning and writing Assembly across different architectures, using llvm
+
+Linux syscall reference: [syscalls.mebeim.net](https://syscalls.mebeim.net/?table=x86/64/x64/latest)
+
+## Running
+
+Build: `clang -Wall -g -masm=intel -c {file}.asm -o build/main.o`
+
+Link: `ld.lld -o build/main build/main.o`
---
+++ x64/hello_world/hello_world.asm
@@ -0,0 +1,34 @@
+# hello world in x64 assembly
+
+# entry point
+.global _start
+
+# label, not function, but naming convenience
+_start:
+    mov rdi, 19 # length of string
+    lea rsi, l_hi # load address of string into rsi
+    call fn_print
+    mov rax, 0 # prepare to exit
+    mov rdi, 0 # set exit code
+    jmp fn_exit
+
+# parameters:
+#   rdi: length of string
+#   rsi: string to print
+fn_print:
+    # prepare to syscall write
+    mov rax, 1
+    # write takes fd in rdi, buf in rsi, and count in rdx
+    mov rdx, rdi
+    mov rdi, 1
+    syscall # now execute
+    ret
+
+# does what it says
+# prefer to set exit code where you have context
+fn_exit:
+    mov rax, 60
+    syscall
+
+l_hi:
+    .ascii "Hello, from world!\n"
