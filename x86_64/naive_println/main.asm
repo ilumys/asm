6e55c7da1e0e853f1a7e72baa53ac7349970263f
skyaadi <vcs@skyaa.dev>
2025-04-25 20:20:45 +1000

    amd64: add strlen and naive println implementations

---
+++ x86_64/naive_println/main.asm
@@ -0,0 +1,26 @@
+BITS 64
+
+GLOBAL _start
+
+%include 'println.asm'
+
+SECTION .data
+
+; continue to append NULL, as this 'append' hides two `write`s
+; so it's not a real append by any means, but for appearances
+; and learning to work with pointers, it's a start
+str0: db "good day sir", 0x0
+str1: db "having fun with memory?", 0x0
+
+SECTION .text
+
+_start:
+        lea rsi, str0
+        call fn_println
+        lea rsi, str1
+        call fn_println
+
+        ; exit
+        mov rax, 0x3c
+        mov rdi, 0x0
+        syscall
---
+++ x86_64/strlen/main.asm
@@ -0,0 +1,47 @@
+BITS 64
+
+GLOBAL _start
+
+SECTION .data
+str: db "string of some length", 0xa, 0x0 ; terminate with NULL byte
+
+SECTION .text
+
+_start:
+        lea rdx, str        ; load pointer into rdx
+        call fn_strlen      ; call function
+
+        mov rax, 0x1        ; syscall = write
+        lea rsi, str
+        mov rdi, 0x1        ; fd = stdout
+        syscall
+
+        mov rax, 0x3c       ; syscall = exit
+        mov rdi, 0x0
+        syscall
+
+; fn_strlen
+; Takes a pointer to a string and returns its length in bytes
+;
+; Arguments:
+; rdx    string pointer
+;
+; Returns:
+; rdx    number of bytes
+;
+fn_strlen:
+        mov r11, rdx        ; copy pointer from rdx to r11
+
+_nextchar:                  ; a poor man's for loop
+        cmp byte [rdx], 0x0 ; compare current byte to NULL
+        jz _calculate       ; if byte[rsi] and 0x0 are equal, jump
+        inc rdx             ; else, move to next byte
+        jmp _nextchar       ; repeat
+
+_calculate:
+        sub rdx, r11        ; pointer arithmetic. subtract addr in r11 (which
+                            ; points to the zeroth byte) from rdx (which has
+                            ; been incremented to the last byte), finally
+                            ; returning the number of bytes used
+        ret
+; end fn_strlen
